d27f977a34229d467c415809d2ff98b99d66e687
JulianDucharme <bigbluehusky@outlook.com>
2017-04-19 16:58:39 -0500

    Hello, here are my files for the app -Julian

---
+++ app/src/main/java/edu/uta/softeng/anappforjack/UserAreaActivity.java
@@ -0,0 +1,12 @@
+package edu.uta.softeng.anappforjack;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_area);
+    }
+}
