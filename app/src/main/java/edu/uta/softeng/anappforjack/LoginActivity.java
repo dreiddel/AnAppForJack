0aa97f39ced000e3824952b57d8294ba11714e65
JulianDucharme <bigbluehusky@outlook.com>
2017-04-20 12:19:31 -0500

    Hello, here are my files for the app -Julian

--- app/src/main/java/edu/uta/softeng/anappforjack/LoginActivity.java
+++ app/src/main/java/edu/uta/softeng/anappforjack/LoginActivity.java
@@ -1,6 +1,9 @@
 package edu.uta.softeng.anappforjack;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final android.widget.EditText etPassword = (android.widget.EditText) findViewById(R.id.etPassword);
+        final android.widget.EditText etUsername = (android.widget.EditText) findViewById(R.id.etUsername);
+        final android.widget.Button button2 = (Button) findViewById(R.id.button2);
+        final android.widget.TextView registerLink = (TextView)
+
+        registerLink.SetOnClickListener(new View.OnClickListener()
+
     }
 }
--- app/src/main/java/edu/uta/softeng/anappforjack/RegisterActivity.java
+++ app/src/main/java/edu/uta/softeng/anappforjack/RegisterActivity.java
@@ -1,6 +1,7 @@
 package edu.uta.softeng.anappforjack;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -8,5 +9,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final android.widget.EditText etPasswordCheck = (android.widget.EditText) findViewById(R.id.etPasswordCheck);
+        final android.widget.EditText etPassword = (android.widget.EditText) findViewById(R.id.etPassword);
+        final android.widget.EditText etUsername = (android.widget.EditText) findViewById(R.id.etUsername);
+        final android.widget.EditText etemail = (android.widget.EditText) findViewById(R.id.etemail);
+
+        final android.widget.Button button = (Button) findViewById(R.id.button);
     }
 }
