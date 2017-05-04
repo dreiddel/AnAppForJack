b52fc2de3e8c517f88bb87e291e7a7b309e6dc65
DSKotzer <dominickotzer@gmail.com>
2017-05-04 08:27:44 -0500

    Changed a bunch of UI stuff and made it subjectively prettier

--- app/src/main/java/edu/uta/softeng/anappforjack/NavigationAppCompatActivity.java
+++ app/src/main/java/edu/uta/softeng/anappforjack/NavigationAppCompatActivity.java
@@ -2,13 +2,16 @@ package edu.uta.softeng.anappforjack;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Daakru Akaigia on 4/30/2017.
@@ -78,59 +81,94 @@ public class NavigationAppCompatActivity extends AppCompatActivity {
     }
     // + NAVIGATION FUNCTIONALITY
     public void navigationGoToLogin(View view){
-        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
-        Intent intent = new Intent (this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+        final Intent intent = new Intent (this, LoginActivity.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Successfully Logged Out", Toast.LENGTH_SHORT).show();
+            }
+        }, 200);
     }
 
     public void navigationGoToRegister(View view){
-        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
-        Intent intent = new Intent (this, RegisterActivity.class);
-        startActivity(intent);
+        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final Intent intent = new Intent (this, RegisterActivity.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                mDrawerLayout.closeDrawer(Gravity.LEFT, false);
+            }
+        }, 400);
     }
 
     public void navigationGoToUserArea(View view){
-        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
-        Intent intent = new Intent (this, UserAreaActivity.class);
-        startActivity(intent);
+        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final Intent intent = new Intent (this, UserAreaActivity.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                mDrawerLayout.closeDrawer(Gravity.LEFT, false);
+            }
+        }, 400);
     }
 
     public void navigationGoToAccount(View view){
-        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
-        Intent intent = new Intent (this, Account.class);
-        startActivity(intent);
+        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final Intent intent = new Intent (this, Account.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                mDrawerLayout.closeDrawer(Gravity.LEFT, false);
+            }
+        }, 400);
     }
     /*
     public void navigationGoToInterest(View view){
         DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
         Intent intent = new Intent (this, Interest.class);
         startActivity(intent);
+        mDrawerLayout.closeDrawers();
     }
     */
     public void navigationGoToEvents(View view){
-        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
-        Intent intent = new Intent (this, EventActivity.class);
-        startActivity(intent);
+        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final Intent intent = new Intent (this, EventActivity.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                mDrawerLayout.closeDrawer(Gravity.LEFT, false);
+            }
+        }, 400);
     }
     public void navigationGoToFriends(View view){
-        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
-        Intent intent = new Intent (this, FriendList.class);
-        startActivity(intent);
+        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final Intent intent = new Intent (this, FriendList.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                mDrawerLayout.closeDrawer(Gravity.LEFT, false);
+            }
+        }, 400);
     }
     /*
     public void navigationGoToMessaging(View view){
         DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.closeDrawers();
         Intent intent = new Intent (this, Messaging.class);
         startActivity(intent);
+        mDrawerLayout.closeDrawers();
     }
     */
     // - NAVIGATION FUNCTIONALITY
